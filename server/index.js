93335180398198cc836275c3d771fd51e0cbbea5
Juan Hoyos <juanhoyos1347@gmail.com>
2022-11-14 02:35:08 -0500

    swagger api docs and validators code

--- server/index.js
+++ server/index.js
@@ -6,11 +6,32 @@ require('dotenv').config();
 const ticketsRouter = require('./v1/routes/ticket.route.js');
 const login = require('./v1/routes/user.route');
 const usuarios = require('./v1/routes/usuarios.route');
+const path = require("path");
+//Swagger
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+
+const swaggerSpec = {
+    definition:{
+        openapi:"3.0.0",
+        info:{
+            title:"MySupport API",
+            version:"1.0.0"
+        },
+        servers:[
+            {
+                url:'https://mysupport-production.up.railway.app/'
+            }
+        ],
+    },
+    apis: [`${path.join(__dirname,"./v1/routes/*.js")}`]
+}
+
 //Middlewares
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-
+app.use("/v1/doc",swaggerUI.serve,swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
 app.use('/v1/tickets',ticketsRouter);
 app.use('/v1/login', login);
 app.use('/v1/user', usuarios);
