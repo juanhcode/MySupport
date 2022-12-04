b954af2e248f8b1d8035a92b23cb859555013922
Johan Diaz <vasquez.johan@correounivalle.edu.co>
2022-12-03 19:19:30 -0500

    creacion supervisorJS

--- Client/user/public/assets/js/login.js
+++ Client/user/public/assets/js/login.js
@@ -1,46 +1,46 @@
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const form = document.querySelector('form');
-const message = document.querySelector('.message');
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const form = document.querySelector('form')
+const message = document.querySelector('.message')
 
 form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const user = {
-        email: email.value,
-        password: password.value
-    }
-    const newUser = JSON.stringify(user);
-    await login(newUser);
+  e.preventDefault()
+  const user = {
+    email: email.value,
+    password: password.value,
+  }
+  const newUser = JSON.stringify(user)
+  await login(newUser)
 })
 
 const login = async (user) => {
-    try {
-        const response = await fetch("http://localhost:4000/v1/login", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: user
-        });
-        const data = await response.json();
-        const rol = data.data?.rol;
-        const { tokenSession } = data
-        console.log(tokenSession)
-        localStorage.setItem("token", tokenSession)
-        if (response.status == 404) {
-            message.textContent = data?.error;
-        }
-        if (response.status == 409) {
-            message.textContent = data?.error;
-        }
-        if (response.status == 200 && rol == 'administrador') {
-            window.location.href = "../../../../../Client/user/public/index.html";
-        }else if(response.status == 200 && rol == 'empleado'){
-            window.location.href = '../../../../../Client/user/employee/index.html'
-        }else if(response.status == 200 && rol == 'supervisor'){
-            //window.location.href = '../../../../../Client/user/employee/index.html'
-        }
-    } catch (error) {
-        console.log(error);
+  try {
+    const response = await fetch('http://localhost:4000/v1/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: user,
+    })
+    const data = await response.json()
+    localStorage.setItem('user', JSON.stringify(data?.data))
+    const rol = data.data?.rol
+    const { tokenSession } = data
+    localStorage.setItem('token', tokenSession)
+    if (response.status == 404) {
+      message.textContent = data?.error
     }
+    if (response.status == 409) {
+      message.textContent = data?.error
+    }
+    if (response.status == 200 && rol == 'administrador') {
+      window.location.href = '../../../../../Client/user/public/index.html'
+    } else if (response.status == 200 && rol == 'empleado') {
+      window.location.href = '../../../../../Client/user/employee/index.html'
+    } else if (response.status == 200 && rol == 'supervisor') {
+      //window.location.href = '../../../../../Client/user/employee/index.html';
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
